ea809abcf3487d8b02e2f919ba0b98a1b156ed86
Rahulwagh07 <rahulwagh3774@gmail.com>
2024-10-07 03:45:57 +0530

    fix suspence

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,22 +1,24 @@
-"use client";   
+"use client"
+import { Suspense } from "react";
 import LoginModal from "@/components/auth/login-modal";
 import SignupModal from "@/components/auth/signup-modal";
 import { useSession } from "next-auth/react";
-import {useRouter, useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import ForgotPassword from "@/components/auth/forgot-password";
- 
 
-export default function AuthPage() {
+function AuthContent() {
   const session = useSession();
   const router = useRouter();
   const searchParams = useSearchParams();
-  const modal = searchParams.get('modal')
- 
+  const modal = searchParams.get('modal');
+
   if (session.status === "authenticated") {
     router.push("/");
+    return null;
   }
   else if (session.status == "unauthenticated" && modal === null) {
     router.push("/");
+    return null;
   }
   
   return (
@@ -25,6 +27,13 @@ export default function AuthPage() {
       {modal === 'signup' && <SignupModal />}
       {modal === 'forgot-password' && <ForgotPassword />}
     </>
-  )
+  );
 }
- 
+
+export default function AuthPage() {
+  return (
+    <Suspense fallback={<></>}>
+      <AuthContent />
+    </Suspense>
+  );
+}
